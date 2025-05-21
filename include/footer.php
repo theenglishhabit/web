e2632c1c7beeedc89287b2c51a8e778a0694e37e
Marcos Huck <marcos@huck.com.ar>
2025-05-21 19:47:05 -0300

    Deploy v2

---
+++ include/footer.php
@@ -0,0 +1,51 @@
+ <div style="padding-bottom: 50px;"  class="footer-background">
+    <footer class="u-clearfix u-footer footer-background" id="sec-5124"><div class="u-clearfix u-sheet u-sheet-1" >
+        <div class="data-layout-selected u-clearfix u-expanded-width u-gutter-30 u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+			<div class="u-align-left u-container-align-left u-container-style u-layout-cell u-right-cell u-size-20 u-size-30-md u-layout-cell-3">
+                <div class="u-container-layout u-valign-top u-container-layout-3">
+                  <img src="images/logo.png" style="margin-top:50px; max-width:60%;">
+                </div>
+              </div>
+              <div class="u-align-left u-container-align-left u-container-style u-layout-cell u-left-cell u-size-20 u-size-30-md u-layout-cell-1">
+                <div class="u-container-layout u-valign-top u-container-layout-1">
+                  <h5 class="u-text u-text-1">Links</h5>
+                  <u class="u-small-text u-text u-text-variant u-text-2" style=" text-decoration:none;">
+				    <li style="list-style-type: none; text-decoration:none; margin-bottom:8px;"><a href="index.php" style=" text-decoration:none;">Inicio</a></li>
+					<li style="list-style-type: none; text-decoration:none; margin-bottom:8px;"><a href="nosotros.php" style=" text-decoration:none;">Nosotros</a></li>
+					<li style="list-style-type: none; text-decoration:none;"><a href="contacto.php" style=" text-decoration:none;">Contacto</a></li>
+				  </u>
+                </div>
+              </div>
+              <div class="u-align-left u-container-align-left u-container-style u-layout-cell u-size-20 u-size-30-md u-layout-cell-2">
+                <div class="u-container-layout u-valign-top u-container-layout-2">
+                  <h5 class="u-text u-text-1">Servicios</h5>
+                  <u class="u-small-text u-text u-text-variant u-text-2" style=" text-decoration:none;">
+				    <li style="list-style-type: none; text-decoration:none;  margin-bottom:8px;"><a href="capacitacion.php" style=" text-decoration:none;">Capacitación</a></li>
+					<li style="list-style-type: none; text-decoration:none;  margin-bottom:8px;"><a href="evaluacion.php" style=" text-decoration:none;">Evaluación</a></li>
+					<li style="list-style-type: none; text-decoration:none;"><a href="interpretacion_traduccion.php" style=" text-decoration:none;">Interpretación y Traducción</a></li>
+				  </u>
+                </div>
+              </div>
+              <div class="u-align-left u-container-align-left u-container-style u-layout-cell u-right-cell u-size-20 u-size-30-md u-layout-cell-3">
+                <div class="u-container-layout u-valign-top u-container-layout-3">
+                  <h5 class="u-text u-text-1">Contacto</h5>
+				  <u class="u-small-text u-text u-text-variant u-text-2" style=" text-decoration:none;">
+				    <li style="list-style-type: none; text-decoration:none; margin-bottom:8px;"><a href="mailto:info@englishhabit.com.ar" style=" text-decoration:none;">E-mail: info@englishhabit.com.ar</a></li>
+					<li style="list-style-type: none; text-decoration:none; margin-bottom:8px;"><a href="tel:+5491168571178" style=" text-decoration:none;">Tel.: +54 9 11-6857-1178</a></li>
+					<li style="list-style-type: none; text-decoration:none;"><a href="https://www.linkedin.com/company/the-english-habit" style=" text-decoration:none;"><img src="images/linkedin-logo.png" width="20"></a> | <a href="https://www.instagram.com/the.english.habit/" style=" text-decoration:none;"><img src="images/instagram-logo.png" width="20"></a> | <a href="https://api.whatsapp.com/send?phone=+5491168571178" style=" text-decoration:none;"><img src="images/whatsapp-logo.png" width="20"></a> | <a href="https://medium.com/@theenglishhabit" style="color: #111111;"><img src="images/medium-logo.png" style="width:20px;"></a>  | <a href="https://whatsapp.com/channel/0029VaV6Q3l42DcYEgibSR0a" style=" text-decoration:none;"><img src="images/whatsapp-channel-logo.png" width="28"></a></li>
+				  </u>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+	  </footer>
+	  </div>
+
+	
+	<a href="https://api.whatsapp.com/send?phone=" target="_blank" class="boton-whatsapp" style="z-index:1000; cursor: pointer !important;" onclick="_gaq.push(['_trackEvent', 'Contacto', 'clic', 'whatsapp']);"><img src="images/logo-whatsapp.png" style="z-index:1000; width:80px; cursor: pointer !important;"></a>
+  
+</body></html>
---
+++ include/header-2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="es"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+	<title>Servicios de Idiomas para empresas | English Habit</title>
+	<meta name="description" content="En The English Habit, nos especializamos en crear soluciones lingüísticas personalizadas para cada empresa, transformando el aprendizaje en una experiencia significativa y útil en contextos reales.">
+    <meta name="keywords" content="English Habit, soluciones lingüisticas, servicios de idiomas, servicios de idiomas para empresas, capacitaciones, traducciones, evaluaciones">
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+    <link rel="stylesheet" href="Destino.css" media="screen">
+	<link rel="stylesheet" href="index.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="English Habit">
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+    
+
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i|Roboto+Condensed:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Oswald:200,300,400,500,600,700|PT+Sans:400,400i,700,700i">
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/default-logo.png"
+}</script>
+    <meta name="theme-color" content="#3fb7fe">
+    <meta property="og:title" content="Destino">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="es"><header class="u-clearfix u-header u-header" id="sec-2041"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <a href="index.php" class="u-image u-logo u-image-1">
+          <img src="images/logo.jpg" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-dropdown u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-hamburger-link u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+ 
+		  
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1">
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.php" style="padding: 10px 20px;">Inicio</a></li>
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="nosotros.php" style="padding: 10px 20px;">Nosotros</a></li>
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" style="padding: 10px 20px;">Servicios</a>
+			<div class="u-nav-popup">
+			<ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10">
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-white" href="capacitacion.php">Capacitación</a></li>
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-white" href="evaluacion.php">Evaluación</a></li>
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-white" href="interpretacion_traduccion.php">Interpretación y Traducción</a></li>
+			</ul>
+			</div>
+			</li>
+			<li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="contacto.php" style="padding: 10px 20px;">Contacto</a></li>
+			</ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-3">
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php">Inicio</a></li>
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="nosotros.php">Nosotros</a></li>
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="">Servicios</a>
+				<div class="u-nav-popup">
+				<ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10">
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="capacitacion.php">Capacitación</a></li>
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="evaluacion.php">Evaluación</a></li>
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="interpretacion_traduccion.php">Interpretación y Traducción</a></li>
+				</ul>
+			    </div>
+                </li>
+				<li class="u-nav-item"><a class="u-button-style u-nav-link" href="contacto.php">Contacto</a></li>
+			    </ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+      </div></header>
